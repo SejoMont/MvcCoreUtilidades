582e59efe5535449a2214916b0d2db78cd9bf15e
SejoMont <sejo.monterocastro@gmail.com>
2024-02-29 10:46:54 +0100

    Add project files.

---
+++ Controllers/UploadFilesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreUtilidades.Helpers;
+
+public class UploadFilesController : Controller
+{
+    private HelperPathProvider helperPathProvider;
+
+    public UploadFilesController(HelperPathProvider helperPathProvider)
+    {
+        this.helperPathProvider = helperPathProvider;
+    }
+
+    // Vista para subir un archivo
+    public IActionResult SubirFichero()
+    {
+        return View();
+    }
+
+    // Método para manejar la subida de archivos mediante POST
+    [HttpPost]
+    public async Task<IActionResult> SubirFichero(IFormFile fichero)
+    {
+        // Obtener la ruta completa del archivo utilizando HelperPathProvider
+        string path = this.helperPathProvider.MapPath(fichero.FileName, Folders.Uploads);
+
+        // Subir el archivo utilizando Stream
+        using (Stream stream = new FileStream(path, FileMode.Create))
+        {
+            // Copiar el contenido del archivo al stream
+            await fichero.CopyToAsync(stream);
+        }
+
+        // Mensajes de éxito en la vista
+        ViewData["MENSAJE"] = "Fichero subido a " + path;
+
+        // Obtener y mostrar la URL base del servidor
+        string urlServer = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
+        ViewData["TEST"] = urlServer;
+
+        // Obtener y mostrar la URL completa del archivo
+        string urlPath = this.helperPathProvider.MapUrlPath(fichero.FileName, Folders.Uploads);
+        ViewData["URL"] = urlPath;
+
+        return View();
+    }
+
+}
---
+++ Helpers/HelperPathProvider.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+
+namespace MvcCoreUtilidades.Helpers
+{
+    // Enumeración que representa las carpetas utilizadas por los controllers
+    public enum Folders { Images = 0, Facturas = 1, Uploads = 2, Temporal = 3 }
+
+    // Clase que proporciona métodos para mapear rutas y URLs de archivos
+    public class HelperPathProvider
+    {
+        private readonly IServer server;
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        // Constructor que recibe el entorno de host web y el servidor
+        public HelperPathProvider(IWebHostEnvironment hostEnvironment, IServer server)
+        {
+            this.server = server;
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        // Método para mapear la ruta completa del archivo en el sistema de archivos del servidor
+        public string MapPath(string fileName, Folders folder)
+        {
+            string carpeta = GetFolderName(folder);
+            string rootPath = this.hostEnvironment.WebRootPath;
+            string path = Path.Combine(rootPath, carpeta, fileName);
+            return path;
+        }
+
+        // Método para mapear la URL completa del archivo
+        public string MapUrlPath(string fileName, Folders folder)
+        {
+            string carpeta = GetFolderName(folder);
+            var addresses = server.Features.Get<IServerAddressesFeature>().Addresses;
+            string serverUrl = addresses.FirstOrDefault();
+            string urlPath = $"{serverUrl}/{carpeta}/{fileName}";
+            return urlPath;
+        }
+
+        // Método privado para obtener el nombre de la carpeta según la enumeración Folders
+        private string GetFolderName(Folders folder)
+        {
+            return folder switch
+            {
+                Folders.Images => "images",
+                Folders.Temporal => "temp",
+                Folders.Facturas => "facturas",
+                Folders.Uploads => "uploads",
+                _ => throw new ArgumentOutOfRangeException(nameof(folder)),
+            };
+        }
+    }
+}
